18921f8559b2c4b82e0ee34df586e5caba2ef31e
Rob Anderson <roboprog@yahoo.com>
2018-02-11 21:10:35 -0800

    maven voodoo for executable jar using Spark web services library

--- src/main/java/com/roboprogs/spa_demo/App.java
+++ src/main/java/com/roboprogs/spa_demo/App.java
@@ -1,5 +1,7 @@
 package com.roboprogs.spa_demo;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello World");
     }
 }
